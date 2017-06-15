a39331a3e4fb7ab9f45862b404b6f875d59299b9
Joshua Martinez <joshuamartinez@joshuas-mbp.attlocal.net>
2017-06-15 15:40:07 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var questions = [
+{	//property  // value
+	question: "what is 2 + 6?",
+	answers: 8	
+},
+{
+	question: "what is 7 + 7?",
+	answers: 14 	
+},
+{
+	question: "what is 9 +7",
+	answers: 16	
+}
+]
+	// looping through questions
+for ( var i = 0; i < questions.length; i++) 
+		var h2 = document.getElementById("answer0")
+
+
+	
+	
+
+
+
+
+			
+		
+	
+
+
+
